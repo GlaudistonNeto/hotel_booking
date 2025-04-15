9b8449dc3d9f1596c8ee9eb99d3c0fd81eb07070
glaudistonneto <glaudistonson@gmail.com>
2025-04-15 17:08:33 -0300

    Add prices to hotels, add some more generated ticket

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
+from fpdf import FPDF
 import pandas
 
-df = pandas.read_csv("hotels.csv", dtype={"id": str})
+df = pandas.read_csv("articles.csv", dtype={"id": str})
 
 
-class Hotel:
-    def __init__(self, hotel_id):
-        self.hotel_id = hotel_id
-        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze()
-
-    def book(self,):
-        """Book a hotel by changing its availability to no"""
-        df.loc[df["id"] == self.hotel_id, "available"] = "no"
-        df.to_csv("hotels.csv", index=False)
+class Article:
+    def __init__(self, article_id):
+        self.id = article_id
+        self.name = df.loc[df['id'] == self.id, 'name'].squeeze()
+        self.price = df.loc[df['id'] == self.id, 'price'].squeeze()
 
     def available(self):
-        """Check if the available"""
-        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
-        if availability == "yes":
-            return True
+        in_stock = df.loc[df['id'] == self.id, 'in stock'].squeeze()
+        return in_stock
 
 
-class ReservationTicket:
-    def __init__(self, customer_name, hotel_object):
-        self.customer_name = customer_name
-        self.hotel = hotel_object
+class Receipt:
+    def __init__(self, article):
+        self.article = article
 
     def generate(self):
-        content = f"""
-        Thank you for your reservation!
-        Here are you booking data:
-        Name: {self.customer_name}
-        Hotel name: {self.hotel.name}
-        """
-        return content
+        pdf = FPDF(orientation="P", unit="mm", format="A4")
+        pdf.add_page()
 
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Receipt nr.{self.article.id}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Article: {self.article.name}", ln=1)
+
+        pdf.set_font(family="Times", size=16, style="B")
+        pdf.cell(w=50, h=8, txt=f"Price: {self.article.price}", ln=1)
+
+        pdf.output("receipt.pdf")
 
 
 print(df)
-hotel_ID = input("Enter the id of the hotel: ")
-hotel = Hotel(hotel_ID)
-if hotel.available():
-    hotel.book()
-    name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(customer_name=name, hotel_object=hotel)
-    print(reservation_ticket.generate())
+article_ID = input("Choose an article to buy: ")
+article = Article(article_id=article_ID)
+if article.available():
+    receipt = Receipt(article)
+    receipt.generate()
 else:
-    print("Hotel is not free.")
+    print("No such article in stock.")
+
